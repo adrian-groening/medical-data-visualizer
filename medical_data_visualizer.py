fdc21f3949089ba6c1e657298ad41c5e567805e1
adriangroening <groeningadrian@gmail.com>
2025-04-25 01:04:30 -0400

    feat: Filterting for second plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -10,7 +10,7 @@ df['BMI'] = df['weight'] / ((df['height'] / 100) ** 2)
 
 df['overweight'] = (df['BMI'] > 25).astype(int)
 
-print(df)
+#print(df)
 
 
 # 3
@@ -20,25 +20,42 @@ df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
 # Normalize gluc
 df['gluc'] = (df['gluc'] > 1).astype(int)
 
-print(df)
+#print(df)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars= 'cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight',])
+    print(df_cat)
+    
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    print(df_cat)
+
     
 
     # 7
+    #print(df_long)
+
+    fig = sns.catplot(
+        data=df_cat, 
+        kind="bar", 
+        x="variable", 
+        y="total", 
+        hue="value", 
+        col="cardio"
+    )
+
+    print(df_cat)
+    plt.show
 
 
 
     # 8
-    fig = None
+    #fig = 
 
 
     # 9
@@ -48,8 +65,17 @@ def draw_cat_plot():
 
 # 10
 def draw_heat_map():
+    print(df)
     # 11
-    df_heat = None
+    df_heat = df[ (df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] < df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] < df['weight'].quantile(0.975)) ]
+    print('new')
+    print(df_heat)
+    #print("filter__________check")
+    #print(df_heat[(df_heat['ap_lo'] > df_heat['ap_hi']) & (df_heat['height'] < df_heat['height'].quantile(0.025)) & (df['height'] > df['height'].quantile(0.975)) ])
+    #print('original__________')
+    #print(df[(df['ap_lo'] > df['ap_hi']) & (df['height'] < df['height'].quantile(0.025)) & (df['height'] < df['height'].quantile(0.975)) & (df['weight'] < df['weight'].quantile(0.025)) & (df['weight'] > df['weight'].quantile(0.975)) ])
+
+
 
     # 12
     corr = None
@@ -60,13 +86,15 @@ def draw_heat_map():
 
 
     # 14
-    fig, ax = None
+    #fig, ax = None
 
     # 15
 
 
 
     # 16
-    fig.savefig('heatmap.png')
-    return fig
+    #fig.savefig('heatmap.png')
+    #return fig
 
+#draw_cat_plot()
+draw_heat_map()
